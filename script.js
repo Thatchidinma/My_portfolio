3d87ab122508d9262342818c87742e03c3e6d21d
Thatchidinma <ndukubachidinma@gmail.com>
2023-10-17 19:57:44 +0100

    made more changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 
+
 function opentab(tabname){
     for(tablink of tablinks){
         tablink.classList.remove("active-link");
@@ -11,3 +12,11 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 20 || document.body.scrollTop > 20 ) {
+      document.getElementById("btn-back-to-top").hidden = false;
+    } else {
+      document.getElementById("btn-back-to-top").hidden = true;
+    }
+  };
